7a85def7f8d6f4d8fda2e96ed4706e6c3d233962
phil10430 <p.freiburger@web.de>
2022-07-12 08:09:58 +0200

    update stuff

--- post_status.php
+++ post_status.php
@@ -1,16 +1,47 @@
 <?php
 require_once "config.php";
 
+$UserName = $_POST["UserName"];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {    
         $ConStatus = test_input($_POST["ConStatus"]);        
         $UserName = test_input($_POST["UserName"]);   
-        $WishedAction = test_input($_POST["WishedAction"]);  
+          
         $BoxName = test_input($_POST["BoxName"]);  
+        $LockStatus = test_input($_POST["LockStatus"]);
+        $Protection = test_input($_POST["Protection"]);
+        $OpenTime = test_input($_POST["OpenTime"]);    
 
-        $sql = "UPDATE users SET conStatus=$ConStatus, WishedAction='$WishedAction' WHERE username='$UserName'";
-        
+        $sql = "INSERT INTO history (BoxName, LockStatus, Protection, OpenTime)
+        VALUES ('" . $BoxName . "', '" . $LockStatus . "', '" . $Protection . "', '" . $OpenTime . "')";
+
+        $sql = "UPDATE users SET conStatus=$ConStatus,
+         LockStatus='$LockStatus', Protection = '$Protection',
+         OpenTime = '$OpenTime', BoxName = '$BoxName'  WHERE username='$UserName'";
+
+        //optional post item
+        if( isset( $_POST['WishedAction'] )) {
+            $WishedAction = test_input($_POST["WishedAction"]);     
+            $sql = "UPDATE users SET WishedAction='$WishedAction' WHERE username='$UserName'";
+        }
+
         if ($link->query($sql) === TRUE) {
-            echo "New record created successfully";
+           // echo "New record created successfully";
+
+
+            $query = "SELECT WishedAction FROM users WHERE username = '$UserName'";
+            $result = mysqli_query($link, $query);
+            $number_of_rows = mysqli_num_rows($result);
+            $response = array();
+            if($number_of_rows > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $response[] = $row;
+                }
+            }
+          //  header('Content-Type: application/json');
+            echo json_encode(array("users"=>$response));
+
+
         } 
         else {
             echo "Error: " . $sql . "<br>" . $conn->error;
@@ -26,4 +57,7 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+
+    
 ?>
