a025ced0687f8c69b6dec1d2216ab362be3799cf
phil10430 <p.freiburger@web.de>
2022-07-21 14:21:57 +0200

    remove unneccesary files

--- index.php
+++ index.php
@@ -1,77 +1,78 @@
-<?php require "login.php";?>
+<?php require "login.php"; ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-      <!-- <link rel="stylesheet" href="myStyle.css">  -->
+    <!-- <link rel="stylesheet" href="myStyle.css">  -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width; initial-scale=1.0;">
     <title>LockMe</title>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-
-     <!-- DateTimePicker -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
+    
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
     <!-- DateTimePicker -->
 </head>
 
 <body>
-
-    <div id="nav-placeholder"></div>
-
+    <!--  <div id="nav-placeholder"></div> -->
     <script>
         $(function() {
-            $("#nav-placeholder").load("navbar.html");
-            $("#footer-placeholder").load("footer.html");
             $('#datetimepicker1').datetimepicker({
-                minDate : new Date(),
-                format : 'DD/MM/YYYY HH:mm',
+                minDate: new Date(),
+                format: 'DD/MM/YYYY HH:mm',
             });
         });
-        
+
         // prevent post on refresh
-        if ( window.history.replaceState ) {
-            window.history.replaceState( null, null, window.location.href );
-        } 
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
     </script>
-    
-    <div class="main">
 
-        <?php  if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {?>
-             <!-- HTML here -->
-             <h1 class="my-5">Hello <?php echo htmlspecialchars($_SESSION["username"]); ?></h1>
-            
-             <?php  
-             /*
-             if ($_SESSION["role"] == "Master" ){
-                require "setup_master.php";
-                require "setup_master_form.php";
-             }
-             else {
-                require "setup_slave.php";
-             }
-             */
-             require "box_control.php";
-             require "show_history.php";
-             ?>
-         
-            <p>
-                <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-            </p>
-            <p>
-                <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-            </p>
+    <section class="container" id="main">
 
-        <?php } else { require "login_form.php";}?>
-    </div>
+        <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
 
-    <div id="footer-placeholder" class="footer"></div>
+            <div class="card">
 
-</body></html>
+                <div class="card-header">
+                    <h1 class="my-5">Hello <?php echo htmlspecialchars($_SESSION["username"]); ?></h1>
+                </div>
+
+                <div class="card-body">
+                    <?php
+                    require "box_control.php";
+                    require "show_history.php";
+                    ?>
+
+                    <div class="row form-group">
+                        <div class="col-sm">
+                            <a href="logout.php" class="btn btn-danger ml-3">Sign Out</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                </div>
+                <div class="card">
+
+
+                <?php } else {
+                require "login_form.php";
+            } ?>
+
+    </section>
+
+</body>
+
+</html>
