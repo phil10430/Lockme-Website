26277f516653ebfc3d490489c68ba059afc7fe13
phil10430 <p.freiburger@web.de>
2022-07-29 10:03:46 +0200

    bugfix login/logout

--- login_from_app.php
+++ login_from_app.php
@@ -9,13 +9,16 @@ $username_err = $password_err = $login_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($_POST["RequestType"] !== REQUEST_TYPE_LOGOUT){
-        // Check if username is empty
-        if (empty(trim($_POST["username"]))) {
-            $username_err = "Please enter username.";
-        } else {
-            $username = mysqli_real_escape_string($link, trim($_POST["username"]));
-        }
+
+    // Check if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter username.";
+    } else {
+        $username = mysqli_real_escape_string($link, trim($_POST["username"]));
+    }
+
+    if ($_POST["RequestType"] == REQUEST_TYPE_LOGIN) {
+
 
         // Check if password is empty
         if (empty(trim($_POST["password"]))) {
@@ -49,17 +52,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                         if (mysqli_stmt_fetch($stmt)) {
                             if (password_verify($password, $hashed_password)) {
-                              $isLoggedIn = 1;
-                              $query = "UPDATE users SET appLoggedIn=? WHERE username=?";
-                              $stmt = mysqli_prepare($link, $query);
-                              mysqli_stmt_bind_param($stmt, 'is', $isLoggedIn, $username);
-                              mysqli_stmt_execute($stmt);
-                              echo "logged_in";
+                                $isLoggedIn = 1;
+                                $query = "UPDATE users SET appLoggedIn=? WHERE username=?";
+                                $stmt = mysqli_prepare($link, $query);
+                                mysqli_stmt_bind_param($stmt, 'is', $isLoggedIn, $username);
+                                mysqli_stmt_execute($stmt);
+                                echo "logged_in";
                             } else {
                                 echo "Invalid password";
                             }
                         }
-                    } else { 
+                    } else {
                         echo "servername doesn't exist";
                     }
                 } else {
@@ -67,23 +70,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 }
 
                 // Close statement
-                mysqli_stmt_close($stmt);
+               // mysqli_stmt_close($stmt);
             }
-        }else{
+        } else {
             echo 'Username or Password is empty.';
         }
-
-        // Close connection
-        mysqli_close($link);
-    }
-    else{
+    } elseif ($_POST["RequestType"] == REQUEST_TYPE_LOGOUT) {
         //Logout
         $isLoggedIn = 0;
         $query = "UPDATE users SET appLoggedIn=? WHERE username=?";
         $stmt = mysqli_prepare($link, $query);
-        mysqli_stmt_bind_param($stmt, 'is', $isLoggedIn, $username);
+        mysqli_stmt_bind_param($stmt, 'is',  $isLoggedIn, $username);
         mysqli_stmt_execute($stmt);
-            
+
         echo "logged_out";
     }
 }
